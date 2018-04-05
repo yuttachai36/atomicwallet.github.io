7013411a4998926fce30b7a04b7520850e734301
Atomicwallets <36169551+Atomicwallets@users.noreply.github.com>
2018-04-05 03:29:29 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,95 @@
 let qs = q => document.querySelector(q);
 let qsa = q => document.querySelectorAll(q);
 
+/* download */
+class Download {
+    constructor() {
+        this.modalObj = qs('#downloadModal');
+        this.buttonObj = qs('#downloadButton');
+        this.formObj = qs('#downloadForm');
+        this.typeObj = qs('#downloadType');
+        this.emailObj = qs('#downloadEmail');
+        this.html = qs('html');
+
+        this.sending = false;
+
+        if(this.modalObj) {
+            this.binds();
+        }
+    }
+
+    binds() {
+        qsa('[data-download]').forEach(el => {
+            el.onclick = ev => {
+                ev.preventDefault();
+                this.setStep(1);
+                this.openModal(el.dataset.download);
+                return false;
+            };
+        });
+
+        this.modalObj.onclick = el => {
+            if(el.target.classList.contains('modal') || el.target.classList.contains('container')) {
+                this.modalClose();
+            }
+        };
+        qs('[data-download-close]').onclick = _ => this.modalClose();
+
+        // this.formObj.onsubmit = ev => {
+        //     ev.preventDefault();
+        //     this.send();
+        //     return false;
+        // }
+    }
+
+    openModal(type) {
+        // this.typeObj.value = type.toLowerCase();
+        this.modalObj.dataset.downloadType = type.toLowerCase();
+
+        this.html.style.overflow = 'hidden';
+        this.html.style.position = 'fixed';
+        this.modalObj.classList.add('modal-open');
+    }
+
+    modalClose() {
+        this.html.style.overflow = 'auto';
+        this.html.style.position = 'relative';
+        this.modalObj.classList.remove('modal-open')
+    }
+
+    send() {
+        if(this.sending) return;
+
+        this.sending = true;
+        this.buttonObj.setAttribute('disabled', 'disabled');
+
+        fetch(this.formObj.getAttribute('action'), {
+            method: 'POST',
+            body: new FormData(this.formObj)
+        })
+            .then(r => r.json())
+            .then(r => {
+                this.emailObj.value = '';
+                this.sending = false;
+                this.buttonObj.removeAttribute('disabled');
+
+                if(r.status) {
+                    this.setStep(2);
+                } else {
+                    alert(r.message);
+                }
+            });
+    }
+
+    setStep(step) {
+        this.modalObj.classList.remove('modal-step-1');
+        this.modalObj.classList.remove('modal-step-2');
+        this.modalObj.classList.add('modal-step-' + step);
+    }
+}
+new Download();
+/* /download */
+
 document.addEventListener('DOMContentLoaded', _ => {
     /* list */
     let listHide = true,
@@ -104,4 +193,5 @@ document.addEventListener('DOMContentLoaded', _ => {
         qs('#menu-button-open').onclick = MenuOpen;
     }
     /* /menu-mobile */
+
 });
